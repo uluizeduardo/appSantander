b01db58c66d84b8551f6e9a5cf011f7af899dfcd
uluizeduardo <luiz.analistadesistemas@gmail.com>
2021-05-28 19:07:26 -0300

    first commit

---
+++ src/main/java/com/project/appSantander/AppSantanderApplication.java
@@ -0,0 +1,13 @@
+package com.project.appSantander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSantanderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSantanderApplication.class, args);
+	}
+
+}
